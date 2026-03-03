07a3352d5d9fd68c48a33b467aa910a52da3660d
yurinabbs <bianconi.yurinabil@franchettisalviani.net>
2026-03-03 12:16:14 +0100

    Implementazione classe "Classifica"

--- ChiappaLaTalpa/src/chiappalatalpa/Classifica.java
+++ ChiappaLaTalpa/src/chiappalatalpa/Classifica.java
@@ -3,11 +3,29 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package chiappalatalpa;
-
+import java.util.ArrayList;
+import java.util.List;
+import java.io.*;
 /**
  *
  * @author bianconi.yurinabil
  */
 public class Classifica {
     
+    private List<String> punteggi;
+    
+    public Classifica() {
+        punteggi = new ArrayList<>();
+    }
+    
+    public void salvaPunteggio(String nome, int punteggio) {
+        String record = nome + " - " + punteggio;
+        punteggi.add(record);
+    }
+    
+    public void stampaClassifica() {
+        for (String s : punteggi) {
+            System.out.println(s);
+        }
+    }
 }
